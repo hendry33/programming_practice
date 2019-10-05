f5edac9844d2c19c0a125d93bd8282bb3915d546
hendry33 <55319071+hendry33@users.noreply.github.com>
2019-10-05 15:41:51 +0300

    Add files via upload

---
+++ week03/turtle9.py
@@ -0,0 +1,22 @@
+import turtle
+turtle.speed(1)
+def ugolnik(n):
+    for k in range(i):
+        turtle.left(360 / i)
+        turtle.forward(50)
+
+
+turtle.shape('turtle')
+
+for i in range(3,10):
+    turtle.setheading(0)
+    ugolnik(i)
+    turtle.penup()
+    turtle.forward(5*i)
+    turtle.pendown()
+
+
+
+
+
+turtle.done()
